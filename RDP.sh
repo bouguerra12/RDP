017700537861b31c4fa7113b1e1c6900dc30212b
Sandaru Ashen <sandaruashen02@gmail.com>
2021-04-02 23:53:27 +0530

    Terminal Fixed

--- RDP.sh
+++ RDP.sh
@@ -13,10 +13,10 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base xfce4-terminal
+apt install --assume-yes xfce4 desktop-base xfce4-terminal scite
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
-sudo apt install --assume-yes gnome-terminal
+sudo apt remove --assume-yes gnome-terminal
 sudo systemctl disable lightdm.service
 sudo apt install --assume-yes nautilus nano
 sudo apt-get update
@@ -27,7 +27,7 @@ sudo adduser USER chrome-remote-desktop
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here: " CRP
+read -p "Paste Here:	 " CRP
 su - USER -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 1235 \n'
